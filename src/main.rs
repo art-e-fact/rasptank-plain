a22f3eac08c01611cef28c24cb979884b572270e
Eric Platon <zaraki@gmx.com>
2023-05-01 05:17:31 +0100

    Half working caterpillars (forward)
    Code works, but left motor seems misconfigured (same pins as original, and was working with original code though).

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 pub mod subsystems;
 
 // use crate::subsystems::leds::setup;
-use crate::subsystems::caterpillars::Caterpillars;
+use crate::subsystems::caterpillars::setup;
 
 fn main() {
     // let mut leds = setup();
     // leds.demo();
-    let mut c = Caterpillars {
-        left_motor_pin: 14,
-        right_motor_pin: 15,
-    };
-    match c.forward(3, 100) {
-        Ok(duration) => println!("Run completed for {:?} seconds", duration),
+    let caterpillars = setup(
+        4, 14, 15,
+        17, 27, 18,
+        );
+    match caterpillars.expect("Unabled to access caterpillars").forward(3, 0.5) {
+        Ok(duration) => println!("Forward run for {:?} seconds", duration),
         Err(e) => println!("Failure in running forward: {:?}", e),
     }
 }
--- src/subsystems/caterpillars.rs
+++ src/subsystems/caterpillars.rs
@@ -1,24 +1,58 @@
 use std::thread;
 use std::time::Duration;
 
-use rppal::gpio::Gpio;
-use rppal::gpio::Error;
+use rppal::gpio::{Gpio, Error, OutputPin};
 
 pub struct Caterpillars {
-    pub left_motor_pin: u8,
-    pub right_motor_pin: u8,
+    pub left_motor_pin1: OutputPin,
+    pub left_motor_pin2: OutputPin,
+    pub left_motor_pin_en: OutputPin,
+    pub right_motor_pin1: OutputPin,
+    pub right_motor_pin2: OutputPin,
+    pub right_motor_pin_en: OutputPin,
 }
 
 impl Caterpillars {
-    pub fn forward(&mut self, seconds: u64, _speed: u64) -> Result<u64, Error> {
-        let gpio = Gpio::new()?;
-        let mut lm = gpio.get(self.left_motor_pin)?.into_output();
-        let mut rm = gpio.get(self.right_motor_pin)?.into_output();
-        lm.set_high();
-        rm.set_high();
+    pub fn forward(&mut self, seconds: u64, speed: f64) -> Result<u64, Error> {
+        self.left_motor_pin1.set_low();
+        self.left_motor_pin2.set_high();
+        self.left_motor_pin_en.set_pwm_frequency(1000.0, speed);
+        self.right_motor_pin1.set_high();
+        self.right_motor_pin2.set_low();
+        self.right_motor_pin_en.set_pwm_frequency(1000.0, speed);
         thread::sleep(Duration::from_secs(seconds));
-        rm.set_low();
-        lm.set_low();
+        self.left_motor_pin1.set_low();
+        self.left_motor_pin2.set_low();
+        self.left_motor_pin_en.clear_pwm();
+        self.right_motor_pin1.set_low();
+        self.right_motor_pin2.set_low();
+        self.right_motor_pin_en.clear_pwm();
         Ok(seconds)
     }
+}
+
+pub fn setup(lmp1n: u8, lmp2n: u8, lmpenn: u8, rmp1n: u8, rmp2n: u8, rmpenn: u8) -> Result<Caterpillars, Error> {
+    let gpio = Gpio::new()?;
+    let mut lmp1 = gpio.get(lmp1n)?.into_output();
+    lmp1.set_low();
+    let mut lmp2 = gpio.get(lmp2n)?.into_output();
+    lmp2.set_low();
+    let mut lmpen = gpio.get(lmpenn)?.into_output();
+    lmpen.clear_pwm();
+    let mut rmp1 = gpio.get(rmp1n)?.into_output();
+    rmp1.set_low();
+    let mut rmp2 = gpio.get(rmp2n)?.into_output();
+    rmp2.set_low();
+    let mut rmpen = gpio.get(rmpenn)?.into_output();
+    rmpen.clear_pwm();
+    Ok(
+        Caterpillars {
+            left_motor_pin1: lmp1,
+            left_motor_pin2: lmp2,
+            left_motor_pin_en: lmpen,
+            right_motor_pin1: rmp1,
+            right_motor_pin2: rmp2,
+            right_motor_pin_en: rmpen,
+        }
+      )
 }
